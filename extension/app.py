a04bfa2a96d05c2c41fcfb4223ea13592e7ecf82
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2023-03-27 11:55:38 -0400

    Add files via upload

--- extension/app.py
+++ extension/app.py
@@ -1,4 +1,5 @@
 # auxiliary
+from logging import FileHandler, WARNING
 import time
 import sys
 sys.path.insert(0, r'C:\assignments\cs733-nlp\web-page-summarizer')
@@ -23,6 +24,8 @@ from pywinauto import Application
 
 app = Flask(__name__)
 CORS(app)
+file_handler = FileHandler('log.txt')
+file_handler.setLevel(WARNING)
 
 def generate(text, model_path = r't5-model', tokenizer_path = r't5-model'):
   tokenizer = T5Tokenizer.from_pretrained(model_path, return_tensors = 'pt')
@@ -59,8 +62,14 @@ def speak(text = 'Hotels | action | sort && Hotels | human_readable_position | t
     time.sleep(audio.info.length)
     return text
 
+def coroutine_primer(coroutine):
+    def wrap(*args, **kwargs):
+        cr = coroutine(*args, **kwargs)
+        cr.next()
+        return cr
+    return wrap
 
-@app.route('/trigger')
+@app.route('/trigger/')
 def trigger():
     # driver = webdriver.Chrome()
     # url = driver.command_executor._url
@@ -78,6 +87,15 @@ def trigger():
     # url = chrome.top_window().child_window(title = item, control_type = 'Edit').get_value()
     # print(url)
     # text = request.args['text']
+    
+    
+    features = ['has_inner_text', 'has_search_inner_text', 'num_search', 'has_button', 'has_search_attr', 'coordinates']
+    feature_dict = {feature: request.args.get(feature) for feature in features}
+    print('len(features): ',len(features))
+    for k in features.keys():
+        print(k,':',len(features[k]))
+        print(features[k])
+    ############# Use coroutine/generator to convert the parameters into features
     text = ''
     for text in ['Hotels | action | search && Hotels | human_readable_position | top', 'Hotels | action | sort && Hotels | human_readable_position | top-right','Hotels | action | filter && Hotels | human_readable_position | left', 'Results | action | discover && Results | location | center-bottom-right']:
         speak(generate(load(text)))
@@ -92,3 +110,48 @@ if __name__ == '__main__':
     #     speak(generate(load(text)))
     # map(lambda text: speak(generate(text)), load(''))
     trigger()
+
+
+'''
+Looking in indexes: https://pypi.org/simple, https://us-python.pkg.dev/colab-wheels/public/simple/
+Collecting transformers
+  Downloading transformers-4.25.1-py3-none-any.whl (5.8 MB)
+     |████████████████████████████████| 5.8 MB 21.8 MB/s 
+Requirement already satisfied: tqdm>=4.27 in /usr/local/lib/python3.8/dist-packages (from transformers) (4.64.1)
+Requirement already satisfied: packaging>=20.0 in /usr/local/lib/python3.8/dist-packages (from transformers) (21.3)
+Requirement already satisfied: requests in /usr/local/lib/python3.8/dist-packages (from transformers) (2.23.0)
+Requirement already satisfied: pyyaml>=5.1 in /usr/local/lib/python3.8/dist-packages (from transformers) (6.0)
+Requirement already satisfied: regex!=2019.12.17 in /usr/local/lib/python3.8/dist-packages (from transformers) (2022.6.2)
+Collecting tokenizers!=0.11.3,<0.14,>=0.11.1
+  Downloading tokenizers-0.13.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (7.6 MB)
+     |████████████████████████████████| 7.6 MB 66.0 MB/s 
+Requirement already satisfied: numpy>=1.17 in /usr/local/lib/python3.8/dist-packages (from transformers) (1.21.6)
+Collecting huggingface-hub<1.0,>=0.10.0
+  Downloading huggingface_hub-0.11.1-py3-none-any.whl (182 kB)
+     |████████████████████████████████| 182 kB 75.3 MB/s 
+Requirement already satisfied: filelock in /usr/local/lib/python3.8/dist-packages (from transformers) (3.8.0)
+Requirement already satisfied: typing-extensions>=3.7.4.3 in /usr/local/lib/python3.8/dist-packages (from huggingface-hub<1.0,>=0.10.0->transformers) (4.4.0)
+Requirement already satisfied: pyparsing!=3.0.5,>=2.0.2 in /usr/local/lib/python3.8/dist-packages (from packaging>=20.0->transformers) (3.0.9)
+Requirement already satisfied: urllib3!=1.25.0,!=1.25.1,<1.26,>=1.21.1 in /usr/local/lib/python3.8/dist-packages (from requests->transformers) (1.24.3)
+Requirement already satisfied: certifi>=2017.4.17 in /usr/local/lib/python3.8/dist-packages (from requests->transformers) (2022.9.24)
+Requirement already satisfied: idna<3,>=2.5 in /usr/local/lib/python3.8/dist-packages (from requests->transformers) (2.10)
+Requirement already satisfied: chardet<4,>=3.0.2 in /usr/local/lib/python3.8/dist-packages (from requests->transformers) (3.0.4)
+Installing collected packages: tokenizers, huggingface-hub, transformers
+Successfully installed huggingface-hub-0.11.1 tokenizers-0.13.2 transformers-4.25.1
+Looking in indexes: https://pypi.org/simple, https://us-python.pkg.dev/colab-wheels/public/simple/
+Collecting sentencepiece
+  Downloading sentencepiece-0.1.97-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl (1.3 MB)
+     |████████████████████████████████| 1.3 MB 15.7 MB/s 
+Installing collected packages: sentencepiece
+Successfully installed sentencepiece-0.1.97
+Looking in indexes: https://pypi.org/simple, https://us-python.pkg.dev/colab-wheels/public/simple/
+Collecting pytorch
+  Downloading pytorch-1.0.2.tar.gz (689 bytes)
+Building wheels for collected packages: pytorch
+  Building wheel for pytorch (setup.py) ... error
+  ERROR: Failed building wheel for pytorch
+  Running setup.py clean for pytorch
+Failed to build pytorch
+Installing collected packages: pytorch
+    Running setup.py install for pytorch ... error
+'''
