cefe8aa1821fc67317a7314132475db4fe78145a
narayana541996 <61041697+narayana541996@users.noreply.github.com>
2023-03-29 13:53:00 -0400

    Add files via upload
    app.py-
    Updated classify() to return dataframe with predictions.
    Temporarily commented out the generate and speak calls in generate_summary()
    candidates.js-
    Updated extract_search_features() in class searchCandidates to add num_search for 'placeholder' and 'aria-label' attributes.
    Added classifier-models folder with classifier models from Insupport by Javedul et al.

---
+++ app.py
@@ -0,0 +1,130 @@
+# auxiliary
+from logging import FileHandler, WARNING
+import numpy as np ##### Add numpy, pandas, scikit-learn to requirements.txt
+import pandas as pd
+# import sklearn
+import time
+import pickle
+import sys
+sys.path.insert(0, r'C:\assignments\cs733-nlp\web-page-summarizer')
+
+# nlp
+## custom modules
+# from extension.text_generator import generate
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+# speech
+from io import BytesIO
+from gtts import gTTS
+from pygame import mixer
+from mutagen.mp3 import MP3
+
+# web and browser
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from selenium import webdriver
+from pywinauto import Application
+
+app = Flask(__name__)
+CORS(app)
+file_handler = FileHandler('log.txt')
+file_handler.setLevel(WARNING)
+
+def generate(text, model_path = r't5-model', tokenizer_path = r't5-model'):
+  tokenizer = T5Tokenizer.from_pretrained(model_path, return_tensors = 'pt')
+  model = T5ForConditionalGeneration.from_pretrained(tokenizer_path, return_dict = True)
+  model.eval()
+  input_ids = tokenizer.encode(text, return_tensors = 'pt')
+  outputs = model.generate(input_ids)
+  return tokenizer.decode(outputs[0]).strip('<pad>').strip('</s>').strip()
+
+def load(text):
+    ''' Takes in html page and load it's content and return objects as structured data.'''
+    # structured_data = 'animals | action | filter && animals | location | center'
+    # structured_data = 'Hotels | action | sort && Hotels | human_readable_position | top-left'.replace('_',' ').replace('-',' ')
+    structured_data = text
+    print(structured_data)
+    return structured_data
+
+# @app.route('/')
+def speak(text = 'Hotels | action | sort && Hotels | human_readable_position | top-left'):
+    '''converts text to speech'''
+    print('text: ',text)
+    text = text.replace('_',' ').replace('-',' ')
+    print('text: ',text)
+    mp3_file_object = BytesIO()
+    tts = gTTS(text, lang='en')
+    tts.write_to_fp(mp3_file_object)
+    # mp3_file_object.seek(0)
+    audio = MP3(mp3_file_object)
+    mixer.init()
+    mixer.music.load(mp3_file_object,'mp3')
+    # mixer.music.rewind()
+    mixer.music.play()
+    print('freeze...')
+    time.sleep(audio.info.length)
+    return text
+
+def classify(feature_df, filename=r'classifier-models\search_model.sav'):
+    classifier_model = pickle.load(open(filename,'rb'))
+    coordinates = feature_df.pop('coordinates')
+    feature_df['predictions'] = classifier_model.predict(feature_df.values)
+    # print('predictions:\n',feature_df["predictions"],'\n\ncoordinates:\n',coordinates)
+    print(feature_df.head())
+    return feature_df
+
+
+@app.route('/generate_summary/')
+def generate_summary(features = ['has_inner_text', 'has_search_inner_text', 'num_search', 'has_button', 'has_search_attr', 'coordinates']):
+    # driver = webdriver.Chrome()
+    # url = driver.command_executor._url
+    # print(url)
+    # session_id = driver.session_id
+    # driver = webdriver.Remote(url, desired_capabilities={})
+    # driver.close()
+    # driver.session_id = session_id
+    # driver.get(url)
+    # data = request.get_json()
+    # data = jsonify(data)
+    # chrome = Application('uia')
+    # chrome.connect(title_re = ".*Chrome*", visible_only = True)
+    # item = 'Address and search bar'
+    # url = chrome.top_window().child_window(title = item, control_type = 'Edit').get_value()
+    # print(url)
+    # text = request.args['text']
+    
+    feature_dict = {feature: request.args.get(feature) for feature in features}
+    print('feature_dict: ',feature_dict)
+    print('len(feature_dict): ',len(feature_dict))
+    for k in feature_dict.keys():
+        print(k,':',len(feature_dict[k]),'\n', feature_dict[k])
+        feature_dict[k] = feature_dict[k].split()
+        if not feature_dict[k][0].isnumeric():
+            feature_dict[k] = [list(map(float, row.split(','))) for row in feature_dict[k]]
+        else:
+            feature_dict[k] = list(map(float, feature_dict[k]))
+    
+
+        print(k,':',len(feature_dict[k]), feature_dict[k][0])
+        
+    feature_df = pd.DataFrame(feature_dict)
+    print(feature_df.head())
+    # print('info:')
+    # print(feature_df.info())
+    feature_df = classify(feature_df)
+    text = 'generate_summary() called.'
+    # for text in ['Hotels | action | search && Hotels | human_readable_position | top', 'Hotels | action | sort && Hotels | human_readable_position | top-right', 'Hotels | action | filter && Hotels | human_readable_position | left', 'Results | action | discover && Results | location | center-bottom-right']:
+    #     speak(generate(load(text)))
+    return text
+
+
+# if __name__ == '__main__':
+    # app.run()
+    # for text in load(''):
+    #     print(text)
+    # for text in ['Hotels | action | search && Hotels | human_readable_position | top', 'Hotels | action | sort && Hotels | human_readable_position | top-right','Hotels | action | filter && Hotels | human_readable_position | left', 'Results | action | discover && Results | location | bottom-right']:
+    #     speak(generate(load(text)))
+    # map(lambda text: speak(generate(text)), load(''))
+    # trigger()
+
---
+++ candidates.js
@@ -0,0 +1,173 @@
+// 'use strict';
+const url = window.location.href
+const json = JSON.stringify({'url' : url})
+const link = document.createElement('a')
+console.log('json: ',json)
+// import searchCandidates from "./candidates.js"
+// var file = new Blob([json], {type : 'json'})
+// link.href = URL.createObjectURL(file)
+// link.download = 'url.json'
+// link.click()
+// URL.revokeObjectURL(link.href)
+
+// extract the features, load in an array and send them as url arguments
+// make a searchCandidates object and store the features
+
+class searchCandidates {
+    constructor (){
+        this.has_inner_text = []
+        this.has_search_inner_text = []
+        this.num_search = []
+        this.has_button = []
+        this.has_search_attr = []
+        this.coordinates = []
+        this.extract_search_features()
+    }
+
+    push(item, has_inner_text, has_search_inner_text, num_search, has_button, has_search_attr){
+        console.log('pushed: ', item, 'has_inner_text: ', has_inner_text, ' has_search_inner_text: ',has_search_inner_text, 'num_search: ', num_search, has_button, 'has_search_attr: ', has_search_attr)
+        this.has_inner_text.push(has_inner_text? 1 : 0)
+        this.has_search_inner_text.push(has_search_inner_text? 1 : 0)
+        this.num_search.push(num_search)
+        this.has_button.push(has_button? 1 : 0)
+        this.has_search_attr.push(has_search_attr? 1 : 0)
+        this.coordinates.push(this.generateCoordinates(item))
+    }
+    pop(){
+        this.has_inner_text.pop()
+        this.has_search_inner_text.pop()
+        this.num_search.pop()
+        this.has_button.pop()
+        this.has_search_attr.pop()
+        this.coordinates.pop()
+    }
+
+    generateCoordinates(item) {
+        let temp_coordinates = item.getBoundingClientRect()
+        return [temp_coordinates.left, temp_coordinates.top, temp_coordinates.right, temp_coordinates.bottom, temp_coordinates.height, temp_coordinates.width]
+    }
+
+    makeUrlParams(){
+        // Prepare the attributes into url parameters
+        let inner_array_separator = ','
+        let outer_array_separator = '+'
+        
+        let paramCoordinates = this.coordinates.map(item => item.join(inner_array_separator)) // Form string with each inner-array joined by ','
+        console.log('this.coordinates.map: ', paramCoordinates)
+        paramCoordinates = paramCoordinates.join(outer_array_separator) // Form string with each item in the outer array separated by '+'
+        console.log('paramCoordinates: ', paramCoordinates)
+        let paramHasInnerText = this.has_inner_text.join(outer_array_separator)
+        let paramHasSearchInnerText = this.has_search_inner_text.join(outer_array_separator)
+        let paramNumSearch = this.num_search.join(outer_array_separator)
+        let paramHasButton = this.has_button.join(outer_array_separator)
+        let paramHasSearchAttr = this.has_search_attr.join(outer_array_separator)
+        let searchParam = `has_inner_text=${paramHasInnerText}&has_search_inner_text=${paramHasSearchInnerText}&num_search=${paramNumSearch}&has_button=${paramHasButton}&has_search_attr=${paramHasSearchAttr}&coordinates=${paramCoordinates}`
+        
+        console.log('searchParam: ',searchParam)
+        return searchParam
+
+    }
+
+    extract_search_features = function () {
+        'Checks if there is a search keyword in attribute values.'
+        for (const item of document.querySelectorAll('form')) {
+            let has_search_attr = false
+            let has_button = false
+            let has_inner_text = false
+            let has_search_inner_text = false
+            let num_search = 0
+            console.log('attributes: ', item.attributes)
+            for (const att of item.attributes) {
+                // console.log(att.value.toLowerCase())
+                if (att.value.toLowerCase().match('search')) {
+                    // console.log('attribute: ',att)
+                    console.log('value: ',att.value)
+                    has_search_attr = true
+            
+            // check if it has inner text (aria-label and placeholder are also considered inner text)
+            if (item.attributes['aria-label'] != undefined && item.attributes['aria-label'].value.toLowerCase().match('search')) {
+                has_search_inner_text = true
+                num_search += 1
+            }
+            if (item.attributes['placeholder'] != undefined && item.attributes['placeholder'].value.toLowerCase().match('search')) {
+                has_search_inner_text = true
+                num_search += 1
+            }
+            }
+            }
+            for (const child of item.querySelectorAll('*')){
+                for (const child_att of child.attributes) {
+
+                    console.log('child: ',child)
+                    if (child_att.value.toLowerCase().match('search')) {
+                        // console.log('child: ',child)
+                        // console.log('attribute: ',child_att)
+                        // console.log('value: ',child_att.value)
+                        has_search_attr = true
+                        
+                    }
+               
+                }
+                // Check if child has innerText
+                 if (child.innerText) {
+                    // console.log('child: ',child)
+                    // console.log('innerText: ',child.innerText)
+                    has_inner_text = true
+                    if (child.innerText.toLowerCase().match('search')) {
+                        console.log('child: ',child)
+                        // console.log('innerText: ',child.innerText)
+                        has_search_inner_text = true
+                        num_search += 1
+                        console.log('num_search: ', num_search)
+                    }
+                }
+                
+
+                if (child.attributes['aria-label'] != undefined) {
+                    console.log('child:',child)
+                    console.log('aria-label:',child.attributes['aria-label'].value)
+                    has_inner_text = true
+                    if (child.attributes['aria-label'].value.toLowerCase().match('search')) {
+                        has_search_inner_text = true
+                        num_search += 1
+                    }
+                }
+                if (child.attributes['placeholder'] != undefined) {
+                    console.log('child:',child)
+                    console.log('placeholder:',child.attributes['placeholder'].value)
+                    has_inner_text = true
+                    if (child.attributes['placeholder'].value.toLowerCase().match('search')) {
+                        has_search_inner_text = true
+                        num_search += 1
+                    }
+                }
+            }
+        // Check for button in children
+            if (item.querySelector('button')){
+                // console.log('button: ',item.querySelector('button'));
+                // console.log('has button: ', item.querySelector('button'))
+                // console.log('has search attr: ', has_search_attr)
+                has_button = true
+            }
+    
+            if (item.querySelector('input[type = button]')){
+                console.log('input button: ',item.querySelector('input[type = button]'));
+                // console.log('has button: ', item.querySelector('input[type = button]'))
+                // console.log('has search attr: ', has_search_attr)
+                has_button = true
+                
+            }
+            this.push(item, has_inner_text=has_inner_text, has_search_inner_text=has_search_inner_text, num_search=num_search, has_button=has_button, has_search_attr=has_search_attr)
+        }   
+    }
+};
+
+console.log('document title: ', document.title)
+
+const searchCandidateList = new searchCandidates()
+
+
+
+// extract_search_features()
+console.log(searchCandidateList)
+fetch('http://127.0.0.1:5000/generate_summary?'+searchCandidateList.makeUrlParams()).then(response => response.text).then(result => console.log(result)).catch(error => console.log('error', error))
